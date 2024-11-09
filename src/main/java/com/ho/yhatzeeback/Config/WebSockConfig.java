3514105a345db5341c1b9a7d5c6dd64aaca0253e
Hopar7 <mkiu7030@naver.com>
2024-11-10 03:18:26 +0900

    수정

--- src/main/java/com/ho/yhatzeeback/Config/WebSockConfig.java
+++ src/main/java/com/ho/yhatzeeback/Config/WebSockConfig.java
@@ -1,8 +1,9 @@
 package com.ho.yhatzeeback.Config;
 
+import com.ho.yhatzeeback.Handler.WebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.WebSocketHandler;
+
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
